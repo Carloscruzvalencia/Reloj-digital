44a8afe727b9dc3e22d98467dd62b069157cdc81
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-06-26 17:26:22 +0200

    \update\

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,9 @@
-fecha = new Date();
-fdia = fecha.getDay(); //dia de la semana
-fhora = fecha.getHours(); //hora
-fminutos = fecha.getMinutes(); //minutos
-    fsegundos = fecha.getSeconds(); //segundos
-    document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("dia").innerHTML = diadelasemana;
-    document.getElementById("hora").innerHTML = fhora;
-    document.getElementById("minutos").innerHTML = fminutos;
-    document.getElementById("segundos").innerHTML = fsegundos;
-});
-// dias en español
-semana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
-
-// dias en ingles
-semanaIN = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-// dia de la semana fecha actual
-diadelasemana = semana[fdia];
-
-function ftime(){
+function navoutput() {
     fecha = new Date();
     fdia = fecha.getDay(); //dia de la semana
     fhora = fecha.getHours(); //hora
     fminutos = fecha.getMinutes(); //minutos
     fsegundos = fecha.getSeconds(); //segundos
-
     // dias en español
     semana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
 
@@ -33,13 +12,11 @@ function ftime(){
 
     // dia de la semana fecha actual
     diadelasemana = semana[fdia];
-
     document.getElementById("dia").innerHTML = diadelasemana;
     document.getElementById("hora").innerHTML = fhora;
     document.getElementById("minutos").innerHTML = fminutos;
     document.getElementById("segundos").innerHTML = fsegundos;
+    console.log("n")
 }
-function actualizar() { 
-    mihora = ftime(); 
-}
-setInterval(actualizar, 1000);
+
+setInterval("navoutput()", 1000);
